d9d874e8038bacab5978c488e40c67f3d331db4e
JohnB <john.baylor@gmail.com>
2016-03-05 11:55:16 -0800

    Add react and make it sorta work. Some starter files are from the todo app - which may explain the task list.

---
+++ app/client/routes.jsx
@@ -0,0 +1,30 @@
+const {
+  Router,
+  Route,
+  IndexRoute
+} = ReactRouter;
+
+const createHistory = ReactRouter.history.createHistory;
+
+const routes = (
+  <Route path="/" component={AppLoading}>
+    <Route path="*" component={AppNotFound} />
+  </Route>
+);
+
+const router = (
+  <App history={createHistory()} >
+    {routes}
+  </App>
+);
+
+Meteor.startup(function () {
+  if (Meteor.isClient) {
+    window.setTimeout( function () {
+      var domNode = document.getElementById("root");
+      ReactDOM.render(<App >
+          </App>,
+          domNode);
+    }, 1000)
+  }
+});
