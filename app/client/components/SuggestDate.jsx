7c7b3d119a0e4cc579c19b4b9997fdd60d2df2f5
JohnB <john.baylor@gmail.com>
2016-03-07 00:22:32 -0800

    Default the suggested date to the first friday of the next month. Added moment.js to provide date functionality.

--- app/client/components/SuggestDate.jsx
+++ app/client/components/SuggestDate.jsx
@@ -5,6 +5,18 @@ SuggestDate = React.createClass({
         this.setState({date: this.refs.datepicker.getDate()});
         console.log(this.state);
     },
+    firstFriday: function() {
+        const friday_day_of_week = 5;
+        var fFriday = moment().endOf('month');
+        if(fFriday.day() < friday_day_of_week) {
+            fFriday = fFriday.add(friday_day_of_week - fFriday.day(), 'days')
+        } else if(fFriday.day() > friday_day_of_week) {
+            fFriday = fFriday.add(6, 'days')
+        }
+        fFriday = fFriday.format('MM/DD/YYYY');
+        console.log(fFriday);
+        return fFriday;
+    },
     handleInviteTextChange: function(event) {
         this.setState({inviteText: event.target.value});
     },
@@ -14,6 +26,7 @@ SuggestDate = React.createClass({
                 <span className='gameDate'>
                     <DatePicker
                         ref="datepicker"
+                        defaultDate={this.firstFriday()}
                         />
                 </span>
                 <span className='inviteText'>
